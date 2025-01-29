a89b49513332b152d4ec7d23830c52794cd646b9
Sh3dow-ware <u2486896@rts-365.dk>
2025-01-29 14:34:44 +0100

    minor changes

--- src/component/MainContent.tsx
+++ src/component/MainContent.tsx
@@ -1,4 +1,3 @@
-import Main_Image from "../images/main.jpg";
 import '../style/component/MainContent.scss';
 
 export const MainContent = () => {
@@ -7,7 +6,6 @@ export const MainContent = () => {
             <section className="main-content__section">
                 <h2 className="main-content__title">Hey, it's me <span className="main-content__highlight">Magnus</span></h2>
                 <p className="main-content__description">I am a passionate developer.</p>
-                <img className="main-content__image" src={Main_Image} alt="Main_Image" />
             </section>
         </main>
     );
