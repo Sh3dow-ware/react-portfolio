6a08970231e754b0e2f0a6d91b7ff7f704f6d475
Sh3dow-ware <u2486896@rts-365.dk>
2025-01-31 14:45:03 +0100

    Removed dataIcons and moved it into Utility

--- src/component/TechnologyCard.tsx
+++ src/component/TechnologyCard.tsx
@@ -1,23 +1,7 @@
-import {
-    TypeScript_Logo, JavaScript_Logo, HTML_Logo, Python_Logo,
-    CSS_Logo, Lua_Logo, React_Logo, SQLite_Logo, JSON_Logo, Tailwind_Logo
-} from "../utility/LanguageCardImageData.tsx";
-import { IconType } from "react-icons";
+import {dataIcons} from "../utility/LanguageCardImageData.tsx";
 import "../style/component/TechnologyCard.scss";
 import { Link } from 'react-router';
 
-const dataIcons: { [key: string]: { Icon: IconType, url: string } } = {
-    "TS": { Icon: TypeScript_Logo, url: "https://www.typescriptlang.org/" },
-    "JS": { Icon: JavaScript_Logo, url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript" },
-    "CSS": { Icon: CSS_Logo, url: "https://developer.mozilla.org/en-US/docs/Web/CSS" },
-    "HTML": { Icon: HTML_Logo, url: "https://developer.mozilla.org/en-US/docs/Web/HTML" },
-    "Python": { Icon: Python_Logo, url: "https://www.python.org/" },
-    "Lua": { Icon: Lua_Logo, url: "https://www.lua.org/" },
-    "React": { Icon: React_Logo, url: "https://reactjs.org/" },
-    "JSON": { Icon: JSON_Logo, url: "https://www.json.org/json-en.html" },
-    "Tailwind": { Icon: Tailwind_Logo, url: "https://tailwindcss.com/" },
-    "Sql": { Icon: SQLite_Logo, url: "https://www.sqlite.org/" },
-};
 
 export const TechnologyCard = () => {
     return (
