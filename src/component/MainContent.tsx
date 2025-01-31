3a71bf72011942a24685404c0e14260a604dd171
Sh3dow-ware <u2486896@rts-365.dk>
2025-01-31 14:40:49 +0100

    Added typewrite effect

--- src/component/MainContent.tsx
+++ src/component/MainContent.tsx
@@ -1,11 +1,33 @@
-import '../style/component/MainContent.scss';
+import "../style/component/MainContent.scss";
+import {TypeAnimation} from "react-type-animation"
 
 export const MainContent = () => {
     return (
         <main className="main-content">
             <section className="main-content__section">
-                <h2 className="main-content__title">Hey, it's me <span className="main-content__highlight">Magnus</span></h2>
-                <p className="main-content__description"><span className="main-content__passionate">I am a passionate</span> <span className="main-content__developertag">developer</span></p>
+                <h2 className="main-content__title">
+                    Hey, it's me <span className="main-content__highlight">Magnus</span>
+                </h2>
+                <p className="main-content__description">
+                    <TypeAnimation className={"main-content__developertag"}
+                    sequence={[
+                        "I am a passionate developer",
+                        2000,
+                        "I am a passionate creator",
+                        2000,
+                        "I am a passionate designer",
+                        2000,
+                        "I am a passionate coder",
+                        2000,
+                        "I love react",
+                        1500,
+                        "",
+                    ]}
+                    repeat={Infinity}
+                                   wrapper="span"
+                                   speed={10}
+                    ></TypeAnimation>
+                </p>
             </section>
         </main>
     );
