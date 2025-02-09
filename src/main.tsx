66e03c2b620236e8038374ef86c6cbf209afdfc1
Sh3dow-ware <u2486896@rts-365.dk>
2025-02-09 09:24:51 +0100

    Added main.tsx routing will most likely change to tanstack router.

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ createRoot(document.getElementById('root')!).render(
       <BrowserRouter>
           <Routes>
               <Route path="/" element={<Home/>}></Route>
-              <Route path="/details" element={<Details/>}></Route>
+              <Route path="/projects" element={<Details/>}></Route>
           </Routes>
       </BrowserRouter>
   </StrictMode>,
