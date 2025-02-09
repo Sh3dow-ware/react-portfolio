1728412eecadaaf2b35f4d9a25371f8fec788d88
Sh3dow-ware <u2486896@rts-365.dk>
2025-02-09 09:22:54 +0100

    Function to get: getThemeData.

--- src/utility/ThemeData.tsx
+++ src/utility/ThemeData.tsx
@@ -1,8 +1,18 @@
-import { MdOutlineLightMode as LightMode, MdOutlineDarkMode  as DarkMode} from "react-icons/md";
-import {IconType} from "react-icons";
+import { MdOutlineLightMode as LightMode, MdOutlineDarkMode as DarkMode } from "react-icons/md";
+import { IconType } from "react-icons";
 
-// Exported ThemeData
-export const themeData: { [key: string]: IconType } = {
-    "Light": LightMode,
-    "Dark": DarkMode
+interface ThemeProps {
+    key: keyof typeof themeData;
+    type: IconType;
 }
+
+export const themeData = {
+    Light: LightMode,
+    Dark: DarkMode,
+};
+
+export const getThemeData = (key: keyof typeof themeData): ThemeProps => {
+    return { key, type: themeData[key] };
+};
+ // Simplified it to getThemeData, this mean the type-safety can only be Light and Dark.
+
