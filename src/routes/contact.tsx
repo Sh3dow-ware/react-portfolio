68ca94fcdb9fa79bc3be889a09de43e731d24aa5
Sh3dow-ware <u2486896@rts-365.dk>
2025-05-02 10:16:55 +0200

    Added contact.tsx that adds more content to my portfolio

--- src/routes/contact.tsx
+++ src/routes/contact.tsx
@@ -1,9 +1,48 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { MdEmail } from 'react-icons/md'
+import { FaGithub } from 'react-icons/fa'
+import '@style/component/Contact.scss'
+import "@style/main.scss"
+import {NavigationHeader} from "@component/NavigationHeader";
 
 export const Route = createFileRoute('/contact')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/contact"!</div>
+  return (
+      <>
+      <NavigationHeader></NavigationHeader>
+      <div className="contact">
+        <h1 className="contact__title">Contact Me</h1>
+        <p className="contact__text">
+          You can only contact me via one of the following:
+        </p>
+        <ul className="contact__list">
+          <li className="contact__item">
+            <MdEmail className="contact__icon" />
+            <span className="contact__label">Email:</span>{' '}
+            <a
+                href="mailto:randomacc12411@gmail.com"
+                className="contact__link"
+            >
+              randomacc12411@gmail.com
+            </a>
+          </li>
+          <li className="contact__item">
+            <FaGithub className="contact__icon" />
+            <span className="contact__label">GitHub:</span>{' '}
+            <a
+                href="https://github.com/Sh3dow-ware"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="contact__link"
+            >
+              Github
+            </a>
+          </li>
+        </ul>
+      </div>
+      </>
+  )
 }
