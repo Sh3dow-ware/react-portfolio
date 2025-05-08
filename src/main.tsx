55d413071c5dd2ac4b172cec7cc76e258dd8930d
Sh3dow-ware <u2486896@rts-365.dk>
2025-05-08 12:37:15 +0200

    Added notFoundMode to fuzzy incase, if tanstack changes their default to something else.

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import '@fontsource/monaspace-krypton';
 import { routeTree } from './routeTree.gen'
 
 // Create a new router instance
-const router = createRouter({ routeTree })
+const router = createRouter({ routeTree, notFoundMode: "fuzzy" })
 
 // Register the router instance for type safety
 declare module '@tanstack/react-router' {
