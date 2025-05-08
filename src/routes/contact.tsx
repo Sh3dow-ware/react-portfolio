7e519e537cf8eccafbd4089693615dafee82799a
Sh3dow-ware <u2486896@rts-365.dk>
2025-05-08 12:36:34 +0200

    Added useRouter to get the location path to check, whether if it matches it dosent match /contact. then it only provide the <Outlet>, which fixes the error for me.

--- src/routes/contact.tsx
+++ src/routes/contact.tsx
@@ -1,67 +1,70 @@
-import { createFileRoute } from '@tanstack/react-router'
-import { MdEmail } from 'react-icons/md'
-import { FaGithub } from 'react-icons/fa'
+import {createFileRoute, Outlet, useRouter} from '@tanstack/react-router'
+import {MdEmail} from 'react-icons/md'
+import {FaGithub} from 'react-icons/fa'
 import '@style/component/Contact.scss'
 import "@style/main.scss"
 import {NavigationHeader} from "@component/NavigationHeader";
 import {useEffect} from "react";
 import {ThemeProvider} from "../context/ThemeContext";
-import { CrtEffect } from '@component/CrtEffect'
+import {CrtEffect} from '@component/CrtEffect'
 
 export const Route = createFileRoute('/contact')({
-  component: RouteComponent,
+    component: RouteComponent,
 })
 
-
-
 function RouteComponent() {
+    const router = useRouter();
+    const path = router.state.location.pathname;
 
-  useEffect(() => {
-    document.body.classList.add("contact__portfolio-body");
-    document.getElementById("root")?.classList.add("contact__portfolio-root")
-    return () => {
-      document.body.classList.remove("contact__portfolio-body");
-      document.getElementById("root")?.classList.remove("contact__portfolio-root");
-    };
-  }, []);
+    useEffect(() => {
+        document.body.classList.add("contact__portfolio-body");
+        document.getElementById("root")?.classList.add("contact__portfolio-root")
+        return () => {
+            document.body.classList.remove("contact__portfolio-body");
+            document.getElementById("root")?.classList.remove("contact__portfolio-root");
+        };
+    }, []);
+    console.log(path)
+    if (path !== '/contact') {
+        return <Outlet/>
+    }
 
-
-  return (
-      <>
-        <ThemeProvider>
-          <CrtEffect></CrtEffect>
-      <NavigationHeader></NavigationHeader>
-      <div className="contact">
-        <h1 className="contact__title">Contact Me</h1>
-        <p className="contact__text">
-          You can only contact me via one of the following:
-        </p>
-        <ul className="contact__list">
-          <li className="contact__item">
-            <MdEmail className="contact__icon" />
-            <span className="contact__label">Email:</span>{' '}
-            <a
-                href="mailto:randomacc12411@gmail.com"
-                className="contact__link"
-            >
-              randomacc12411@gmail.com
-            </a>
-          </li>
-          <li className="contact__item">
-            <FaGithub className="contact__icon" />
-            <span className="contact__label">GitHub:</span>{' '}
-            <a
-                href="https://github.com/Sh3dow-ware"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="contact__link"
-            >
-              Github
-            </a>
-          </li>
-        </ul>
-      </div>
-        </ThemeProvider>
-      </>
-  )
+    return (
+        <>
+            <ThemeProvider>
+                <CrtEffect></CrtEffect>
+                <NavigationHeader></NavigationHeader>
+                <div className="contact">
+                    <h1 className="contact__title">Contact Me</h1>
+                    <p className="contact__text">
+                        You can only contact me via one of the following:
+                    </p>
+                    <ul className="contact__list">
+                        <li className="contact__item">
+                            <MdEmail className="contact__icon"/>
+                            <span className="contact__label">Email:</span>{' '}
+                            <a
+                                href="mailto:randomacc12411@gmail.com"
+                                className="contact__link"
+                            >
+                                randomacc12411@gmail.com
+                            </a>
+                        </li>
+                        <li className="contact__item">
+                            <FaGithub className="contact__icon"/>
+                            <span className="contact__label">GitHub:</span>{' '}
+                            <a
+                                href="https://github.com/Sh3dow-ware"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="contact__link"
+                            >
+                                Github
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </ThemeProvider>
+        </>
+    )
 }
