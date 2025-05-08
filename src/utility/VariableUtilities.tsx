1625888022ec9fbef07d338d18dd1460164e5e70
Sh3dow-ware <u2486896@rts-365.dk>
2025-05-08 13:27:23 +0200

    Added utility variables to get email, github

---
+++ src/utility/VariableUtilities.tsx
@@ -0,0 +1,3 @@
+// utility, if I ever change email or need to change github.
+export const emailForContact: string = "m.obsidian.code@proton.me"
+export const githubAccountForContact: string = "https://github.com/Sh3dow-ware/react-portfolio/blob/main/src/component/NavigationHeader.tsx"
