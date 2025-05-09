ed65c32f2b7b50197f9467ffa7ff424245799095
Sh3dow-ware <u2486896@rts-365.dk>
2025-05-09 10:04:40 +0200

    Fix for Heroku build

--- build/express-server.js
+++ build/express-server.js
@@ -1,16 +1,14 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var path_1 = require("path");
-var url_1 = require("url");
-var __filename = (0, url_1.fileURLToPath)(import.meta.url);
-var __dirname = path_1.default.dirname(__filename);
-var app = (0, express_1.default)();
-var PORT = process.env.PORT || 3000;
-app.use(express_1.default.static(path_1.default.join(__dirname, 'dist')));
-app.get('*', function (_req, res) {
-    res.sendFile(path_1.default.join(__dirname, 'dist', 'index.html'));
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.use(express.static(path.join(__dirname, 'dist')));
+app.get('*', (_req, res) => {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
-app.listen(PORT, function () {
-    console.log("Server is running on port ".concat(PORT));
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
